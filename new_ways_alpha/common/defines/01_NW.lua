caef3b64b46cc1c7c7fc88e9640e60751b152129
DRAFTik <draft117@mail.ru>
2020-09-12 00:02:58 +0300

    Допил генерика, баго фиксы и иконочки

--- new_ways_alpha/common/defines/01_NW.lua
+++ new_ways_alpha/common/defines/01_NW.lua
@@ -17,3 +17,8 @@ NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
 NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED = 50
 NDefines_Graphics.NGraphics.CAMERA_ZOOM_KEY_SCALE = 0.02
 NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 15.0
+
+-- AI FIX
+NDefines.NAI.RESEARCH_BONUS_FACTOR = 2.0			-- To which extent AI should care about bonuses to research #0.9
+NDefines.NAI.RESEARCH_AHEAD_OF_TIME_FACTOR = 15.0	-- To which extent AI should care about ahead of time penalties to research	#4.0
+NDefines.NAI.VARIANT_UPGRADE_MIN_XP = 100			-- Minimum XP before attempting to create a new variant. #50
